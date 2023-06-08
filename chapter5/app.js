2ca894797bc87e2ef2d9b62a0756d5e7f6fafa2c
King's meow <mingee641@gmail.com>
2023-06-08 22:52:59 +0900

    Express, 간단한 웹앱 만들기

---
+++ chapter5/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello home page!');
+})
+
+app.get('/login', (req, res) => {
+    res.send('Login please');
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
