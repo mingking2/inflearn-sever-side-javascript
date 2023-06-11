ddabb7a67ff9af74b50cce59d3d100429aece0c3
King's meow <mingee641@gmail.com>
2023-06-11 22:29:53 +0900

    서버 측 JS - 웹앱 제작 6 : 코드의 개선

--- app_file.js
+++ app_file.js
@@ -8,45 +8,48 @@ app.set('views', './views_file');
 app.set('view engine', 'jade');
 
 app.get('/topic/new', (req, res) => {
-    res.render('new');
-})
-
-app.get('/topic', (req, res) => {
     fs.readdir('data', (err, files) => {
-        if(err) {
+        if (err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        res.render('view', {topics: files});
-    })
-})
-app.get('/topic/:id', (req, res) => {
-    const id = req.params.id;
+        res.render('new', {topics:files});
+    });
+});
+
+app.get(['/topic', '/topic/:id'], (req, res) => {
     fs.readdir('data', (err, files) => {
-        if(err) {
+        if (err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        fs.readFile('data/'+id, 'utf-8', (err, data) => {
-            if(err) {
-                console.log(err);
-                res.status(500).send('Internal Server Error');
-            }
-            res.render('view', {topics: files, title: id, description: data});
-        }) 
+        const id = req.params.id;
+        if (id) {
+            // id 값이 있을 때
+            fs.readFile('data/' + id, 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Internal Server Error');
+                }
+                res.render('view', { topics: files, title: id, description: data });
+            })
+        } else {
+            // id 값이 없을 때
+            res.render('view', { topics: files, title:'Welcome', description: "Hello, JavaScript for server." });
+        }
     })
-
-
 })
+
+
 app.post('/topic', (req, res) => {
     const title = req.body.title;
     const description = req.body.description;
     fs.writeFile('data/' + title, description, (err) => {
-        if(err) {
+        if (err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        res.send('Success!');
+        res.redirect('/topic/'+title);
     });
 
 })
