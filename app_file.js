bfce98d275d4930a957674daf772713726b21374
King's meow <mingee641@gmail.com>
2023-06-11 21:46:55 +0900

    서버 측 JS - 웹앱 제작 3 : 본문 저장

--- app_file.js
+++ app_file.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
@@ -9,7 +12,16 @@ app.get('/topic/new', (req, res) => {
 })
 
 app.post('/topic', (req, res) => {
-    res.send('Hi, post');
+    const title = req.body.title;
+    const description = req.body.description;
+    fs.writeFile('data/' + title, description, (err) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Success!');
+    });
+
 })
 
 app.listen(3000, () => {
