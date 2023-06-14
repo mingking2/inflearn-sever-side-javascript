a63c58f8e2643abad559c105686903b92e7b1356
King's meow <mingee641@gmail.com>
2023-06-14 13:25:28 +0900

    Server Side JavaScript - session 1~3 - counter

---
+++ app_session.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var session = require('express-session');
+var app = express();
+
+app.use(session({
+    secret: '24535436543@##$@!',
+    resave: false,
+    saveUninitialized: true
+}));
+app.get('/count', function(req, res) {
+    if(req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send('count : ' + req.session.count);
+});
+
+app.listen(3003, function(){
+    console.log('Connected 3003 port!!!');
+});
